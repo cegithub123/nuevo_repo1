9462deaa421f4417e19f65f495bd4dd8c70e0462
Cesar <ce.nino@gmail.com>
2025-10-25 20:21:59 -0500

    cuarto commit

--- Calculadora.py
+++ Calculadora.py
@@ -7,6 +7,7 @@ class Calculadora:
     def restar(self, a, b):
         # Comentario 1
         # Comentario 2
+        # comentario 3
         if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
             raise TypeError('Ambos parámetros deben ser números enteros o flotantes.')
         return a - b
