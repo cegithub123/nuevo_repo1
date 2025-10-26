1cdc449212470b9a8e3eb6e87d6a4ec41867f812
Cesar <ce.nino@gmail.com>
2025-10-25 20:28:52 -0500

    sexto commit

--- Calculadora.py
+++ Calculadora.py
@@ -9,6 +9,7 @@ class Calculadora:
         # Comentario 2
         # comentario 3
         # comentario 4
+        # comentario 5
         if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
             raise TypeError('Ambos parámetros deben ser números enteros o flotantes.')
         return a - b
